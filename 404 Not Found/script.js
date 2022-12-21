7d6b8f696e1905d79f026c579854a818090a372d
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2022-12-21 10:39:51 -0600

    Create script.js

---
+++ 404 Not Found/script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        head: 404,
+        text: 'NOT FOUND',
+        text2: 'Sorry the page not found',
+        text3: 'Go Back Home'
+    }
+})
